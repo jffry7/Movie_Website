2e961b5793176c0337404481da60a95dfe290336
jffry7 <jffry7@gmail.com>
2017-12-06 17:44:39 +0800

    t

--- lecture_material/twilio_test.py
+++ lecture_material/twilio_test.py
@@ -4,9 +4,9 @@ from twilio.rest import Client
 
 
 # Your Account SID from twilio.com/console
-account_sid = "AC373af3a0589b578f38c6471d93e32c46"
+
 # Your Auth Token from twilio.com/console
-auth_token = "a1158797b1e988414539a980b67d8954"
+
 
 client = Client(account_sid, auth_token)
 
