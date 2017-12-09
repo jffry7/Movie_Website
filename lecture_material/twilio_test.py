fa0f5c58390ff6a40f33b7503db9e9007f9b204a
jffry7 <jffry7@gmail.com>
2017-12-09 21:49:24 +0800

    test

--- lecture_material/twilio_test.py
+++ lecture_material/twilio_test.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 
 
 # Your Account SID from twilio.com/console
-
+account_sid = "AC373af3a0589b578f38c6471d93e32c46"
 # Your Auth Token from twilio.com/console
-
+auth_token = ""
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+15558675309",
-    from_="+15017250604",
+    to="+6592777992",
+    from_="+16172945098",
     body="Hello from Python!")
 
 print(message.sid)
